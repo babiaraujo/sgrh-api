678825d9f6ac0bbf5d9530ec3497b35fb579c300
Bárbara Araujo <69531157+babiaraujo@users.noreply.github.com>
2024-07-18 00:03:24 -0300

    feat: ajusting model employee for better catching errors

--- models/Funcionario.js
+++ models/Funcionario.js
@@ -1,13 +1,37 @@
-const Sequelize = require('sequelize');
+const { Sequelize, DataTypes } = require('sequelize');
 const sequelize = require('../config/database');
 
-const Funcionario = sequelize.define('funcionario', {
+const Funcionario = sequelize.define('Funcionario', {
   nome: {
-    type: Sequelize.STRING,
-    allowNull: false
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      notNull: { msg: 'Nome é obrigatório' },
+      notEmpty: { msg: 'Nome não pode ser vazio' }
+    }
   },
-  data_nascimento: Sequelize.DATE,
-  departamento_id: Sequelize.INTEGER
+  data_nascimento: {
+    type: DataTypes.DATE,
+    allowNull: true,
+    validate: {
+      isDate: { msg: 'Data de nascimento deve ser uma data válida' }
+    }
+  },
+  departamento_id: {
+    type: DataTypes.INTEGER,
+    allowNull: true,
+    references: {
+      model: 'departamentos', 
+      key: 'id'
+    },
+    validate: {
+      isInt: { msg: 'Departamento ID deve ser um número inteiro' }
+    }
+  }
+}, {
+  tableName: 'funcionarios', 
+  timestamps: true, 
+  underscored: true 
 });
 
 module.exports = Funcionario;
